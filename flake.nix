ba8cd9a80490c3e60cfd7cc000cf0e4ea6b6fd9f
herm1t <72761718+herm1t0@users.noreply.github.com>
2025-02-22 16:28:59 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,1 +1,31 @@
+{
+  inputs.disko.url = "github:nix-community/disko/latest";
 
+  outputs = { disco }: {
+    disko.devices = {
+      disk = {
+        main = {
+          device = "/dev/some-device"; # Must be overwrited from the commandline
+          type = "disk";
+          content = {
+            type = "gpt";
+            partitions = {
+              boot = {
+                size = "1M";
+                type = "EF02"; # for grub MBR
+              };
+              root = {
+                size = "100%";
+                content = {
+                  type = "filesystem";
+                  format = "ext4";
+                  mountpoint = "/";
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
