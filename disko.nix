f0d8e82ed5ba9a88a7a27d5be55c17c6e9f4c3bd
herm1t <72761718+herm1t0@users.noreply.github.com>
2025-02-22 17:21:18 +0300

    Update disko.nix

--- disko.nix
+++ disko.nix
@@ -1,26 +1,22 @@
 {
-  inputs.disko.url = "github:nix-community/disko/latest";
-
-  outputs = { disko }: {
-    disko.devices = {
-      disk = {
-        main = {
-          device = "/dev/some-device"; # Must be overwrited from the commandline
-          type = "disk";
-          content = {
-            type = "gpt";
-            partitions = {
-              boot = {
-                size = "1M";
-                type = "EF02"; # for grub MBR
-              };
-              root = {
-                size = "100%";
-                content = {
-                  type = "filesystem";
-                  format = "ext4";
-                  mountpoint = "/";
-                };
+  disko.devices = {
+    disk = {
+      main = {
+        device = "/dev/sda";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
               };
             };
           };
